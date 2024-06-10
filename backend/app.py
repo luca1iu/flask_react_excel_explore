b9c94ba6c74caf1ba698cf0211dd1fc93c62d3ef
luca1iu <lucaliu217@outlook.com>
2024-06-10 16:08:19 +0200

    basic function works

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,22 @@
 from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS
 import pandas as pd
+import os
 
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 CORS(app)
+
+upload_folder = 'excelData'
+if not os.path.exists(upload_folder):
+    os.makedirs(upload_folder)
+
+@app.route('/api/hello', methods=['GET'])
+def hello_world():
+    return jsonify(message="Hello from Flask!")
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/upload',methods=['POST'])
 def upload():
@@ -16,7 +29,9 @@ def upload():
         return jsonify({"error": "No selected file"})
 
     if file and file.filename.endswith('.xlsx'):
-        df = pd.read_excel(file)
+        file_path = os.path.join(upload_folder, file.filename)
+        file.save(file_path)
+        df = pd.read_excel(file_path)
         description = df.describe().to_json()
         return description
 
