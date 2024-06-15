4a48d51fd58aa2a955ea2fd31d01a8615f43baec
luca1iu <lucaliu217@outlook.com>
2024-06-15 12:52:57 +0200

    upload works

--- backend/app.py
+++ backend/app.py
@@ -10,15 +10,18 @@ upload_folder = 'excelData'
 if not os.path.exists(upload_folder):
     os.makedirs(upload_folder)
 
+
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
     return jsonify(message="Hello from Flask!")
+
 
 @app.route('/')
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/upload',methods=['POST'])
+
+@app.route('/upload', methods=['POST'])
 def upload():
     if 'file' not in request.files:
         return jsonify({"error": "No file part"})
@@ -31,11 +34,44 @@ def upload():
     if file and file.filename.endswith('.xlsx'):
         file_path = os.path.join(upload_folder, file.filename)
         file.save(file_path)
-        df = pd.read_excel(file_path)
-        description = df.describe().to_json()
-        return description
+        print('file_path:', file_path)
+        print('file name', file.filename)
+        print("message: File uploaded successfully")
+        return jsonify({"message": "File uploaded successfully", "filename": file.filename})
 
     return jsonify({"error": "Invalid file format"})
+
+
+@app.route('/analyze', methods=['POST'])
+def analyze_file():
+    data = request.json
+    filename = data.get('filename')
+
+    if data is None:
+        return jsonify({"error": "data is None"})
+
+    if not filename:
+        return jsonify({"error": "Filename not provided"})
+
+    file_path = os.path.join(upload_folder, filename)
+    if not os.path.exists(file_path):
+        return jsonify({"error": "File not found"})
+
+    df = pd.read_excel(file_path)
+
+    description = df.describe().to_dict()
+    shape = df.shape
+    null_info = df.isnull().sum().to_dict()
+    distinct_values = {col: df[col].nunique() for col in df.columns}
+
+    analysis = {
+        "description": description,
+        "shape": shape,
+        "null_info": null_info,
+        "distinct_values": distinct_values
+    }
+
+    return jsonify(analysis)
 
 
 if __name__ == '__main__':
